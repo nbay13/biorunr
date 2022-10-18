5e5614715e0a4c77bc57c9f194ed186e65f60b34
unknown <nickbayley13@gmail.com>
2022-10-17 17:37:38 -0700

    added helper_functions.R

---
+++ R/helper_functions.R
@@ -0,0 +1,28 @@
+#' @export get.deltas
+get.deltas <- function(df, id, group, comparison, vars, percent = F){
+	filt_df <- df[df[,group] %in% comparison,]
+	temp <- data.frame(df %>%
+	dplyr::filter(get(group) %in% comparison) %>%
+	dplyr::group_by(get(id)) %>%
+	dplyr::summarise(check = all(comparison %in% get(group))))
+	filt_df <- filt_df[filt_df[,id] %in% temp[temp[,2],1],]
+	a <- filt_df[filt_df[,group] == comparison[1],vars, drop = F]
+	b <- filt_df[filt_df[,group] == comparison[2],vars, drop = F]
+	if(percent){
+		delta <- (a-b) / (abs(b))
+	} else {
+		delta <- a - b
+	}
+	colnames(a) <- paste(comparison[1], vars, sep = ".")
+	colnames(b) <- paste(comparison[2], vars, sep = ".")
+	colnames(delta) <- paste("Delta", vars, sep = ".")
+	return(data.frame(filt_df[filt_df[,group] == comparison[1],!colnames(filt_df) %in% c(group, vars), drop = F],a,b,delta))
+}
+#' @export abs.max
+abs.max <- function(x) max(abs(x))
+
+#' @export lin.map
+lin.map <- function(x) (x-min(x))/(max(x)-min(x))
+
+#' @export qin.map
+qin.map <- function(x, q = 0.99){(x-quantile(x,1-q))/(quantile(x, q)-quantile(x, 1-q))}
