47cfa32a91b3d7e76a3cf64ae3b20acb10579cc1
unknown <nickbayley13@gmail.com>
2024-02-14 15:06:23 -0800

    updated to filter on geneset overlap

--- R/FGSEA_functions.R
+++ R/FGSEA_functions.R
@@ -25,7 +25,7 @@ get.MSigDB.genesets <- function(msig_df, genesets = c("KEGG", "REACTOME", "H$"))
 }
 
 #' @export run.FGSEA
-run.FGSEA <- function(rnk, genesets, nproc = 2, minGenes = 3, maxGenes = 5000, reformat = T, filename = F, minP = 1e-20){
+run.FGSEA <- function(rnk, genesets, nproc = 2, minGenes = 3, maxGenes = 5000, minOverlap = 1, reformat = T, filename = F, minP = 1e-20){
 	# make sure package is loaded
 	gsea_res <- fgsea::fgsea(stats = rnk, pathways = genesets, minSize = minGenes, maxSize = maxGenes, eps = minP, nproc = nproc)
 	## order results based on NES
@@ -36,15 +36,24 @@ run.FGSEA <- function(rnk, genesets, nproc = 2, minGenes = 3, maxGenes = 5000, r
 	})
 	if(reformat){
 		ord_gsea_res$leadingEdge <- new_edge
+		ord_gsea_res$overlap <- lengths(regmatches(ord_gsea_res$leadingEdge, gregexpr(",", ord_gsea_res$leadingEdge))) + 1
+		ord_gsea_res <- ord_gsea_res[ord_gsea_res$overlap >= minOverlap,]
 		if(is.character(filename)){
 			cat(paste0("\nWriting results to file: ", filename, "\nat: ", getwd(), "\n"))
 			write.table(ord_gsea_res, filename, sep = "\t", 
 				quote = F, row.names = F, col.names = T)
 		}
 	} else {
+		ord_gsea_res$overlap <- lengths(ord_gsea_res$leadingEdge)
+		ord_gsea_res <- ord_gsea_res[ord_gsea_res$overlap >= minOverlap,]
 		if(is.character(filename)){
 			cat("\nError: If writing to file reformat must be set to TRUE")
 		}
 	}
 	return(ord_gsea_res)
+}
+
+#' @export filter.FGSEA
+filter.FGSEA <- function(res, minOverlap){
+	return(res[res$overlap >= minOverlap,])
 }
