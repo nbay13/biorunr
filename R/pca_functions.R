313749a9a14d510cce29525a9b73a7ec0372415d
unknown <nickbayley13@gmail.com>
2024-02-23 10:35:36 -0800

    added ellipses to pca plotting

--- R/pca_functions.R
+++ R/pca_functions.R
@@ -1,5 +1,5 @@
 #' @export plot.pca
-plot.pca <- function(pca, meta, color_var = NA, fill_var = NA, shape_var = NA, plot_pcs = c("PC1", "PC2"), color_pal = NA){
+plot.pca <- function(pca, meta, color_var = NA, fill_var = NA, shape_var = NA, plot_pcs = c("PC1", "PC2"), color_pal = NA, ellipse = T, size = 1){
 	if(length(intersect(rownames(pca$x), rownames(meta))) != length(union(rownames(pca$x), rownames(meta)))){
 		stop("Your PCA data and metadata rownames do not match :(\n  Please check your inputs")
 	}
@@ -10,10 +10,18 @@ plot.pca <- function(pca, meta, color_var = NA, fill_var = NA, shape_var = NA, p
 	prop_vars <- round(vars / sum(vars) * 100,2)
 	names(prop_vars) <- paste0("PC", seq(1:length(prop_vars)))
 	pca_df <- data.frame(meta,pca$x[,plot_pcs])
-	gg <- ggplot(pca_df, aes_string(x = plot_pcs[1], y = plot_pcs[2], color = color_var)) + geom_point() + theme_classic() + stat_ellipse() +
-	labs(x = paste0(plot_pcs[1], " (", prop_vars[plot_pcs[1]], "%)"), y = paste0(plot_pcs[2], " (", prop_vars[plot_pcs[2]], "%)")) +
-	scale_color_manual(values = color_pal) + guides(fill = guide_legend(override.aes = list(color = "transparent"))) +
-	theme(axis.text = element_text(size = 10, color = "black"))
+	if(ellipse){
+		gg <- ggplot(pca_df, aes_string(x = plot_pcs[1], y = plot_pcs[2], color = color_var)) + geom_point(size = size) + theme_classic() + stat_ellipse() +
+			labs(x = paste0(plot_pcs[1], " (", prop_vars[plot_pcs[1]], "%)"), y = paste0(plot_pcs[2], " (", prop_vars[plot_pcs[2]], "%)")) +
+			scale_color_manual(values = color_pal) + guides(fill = guide_legend(override.aes = list(color = "transparent"))) +
+			theme(axis.text = element_text(size = 10, color = "black"))
+	} else {
+		gg <- ggplot(pca_df, aes_string(x = plot_pcs[1], y = plot_pcs[2], color = color_var)) + geom_point(size = size) + theme_classic() + 
+			labs(x = paste0(plot_pcs[1], " (", prop_vars[plot_pcs[1]], "%)"), y = paste0(plot_pcs[2], " (", prop_vars[plot_pcs[2]], "%)")) +
+			scale_color_manual(values = color_pal) + guides(fill = guide_legend(override.aes = list(color = "transparent"))) +
+			theme(axis.text = element_text(size = 10, color = "black"))
+	}
+
 	print(gg)
 	return(gg)
 }
