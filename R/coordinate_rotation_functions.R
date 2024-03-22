4235c6ccfa2f4462a6b292cf2ca9a78c8fcacce4
unknown <nickbayley13@gmail.com>
2024-03-22 16:51:44 -0700

    fix

--- R/coordinate_rotation_functions.R
+++ R/coordinate_rotation_functions.R
@@ -1,10 +1,10 @@
 
 #' @export get.rotation.angle
-get.rotation.angle <- function(coord_df, group_var, degree = TRUE){
+get.rotation.angle <- function(coord_df, group_var, col_names = c("dim1", "dim2"), degree = TRUE){
 	if(length(unique(coord_df[[group_var]])) > 2) {
 		print("This function only works for two groups")
 	} else {
-		centr_df <- data.frame(coord_df %>% group_by(get(group_var)) %>% summarise(across(c("dim1", "dim2"), mean)))
+		centr_df <- data.frame(coord_df %>% group_by(get(group_var)) %>% summarise(across(col_names, mean)))
 		angle <- atan((centr_df[1,3] - centr_df[2,3]) / (centr_df[1,2] - centr_df[2,2]))
 		if(degree) angle <- angle * 180 / pi
 	}
@@ -28,7 +28,7 @@ get.rotated.coords <- function(coord_df, rotation_matrix, col_names = c("dim1", 
 
 #' @export rotate.coords
 rotate.coords <- function(coord_df, group_var, col_names = c("dim1", "dim2"), degree = TRUE){
-	angle <- get.rotation.angle(coord_df, group_var, degree = degree)
+	angle <- get.rotation.angle(coord_df, group_var, col_names, degree = degree)
 	# why negative?!
 	rot_mat <- get.rotation.matrix(-angle, degree = degree)
 	rot_coords <- get.rotated.coords(coord_df, rot_mat, col_names = col_names)
